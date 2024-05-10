8aa1b71a7f27415c06ff99df3a3c86e9a308958f
fahadalijs <fahad9827022@gmail.com>
2024-05-10 10:22:58 +0500

    Swtich case

---
+++ app.js
@@ -0,0 +1,40 @@
+// console.log(Switch-case);
+
+
+
+
+
+let grade = "A"
+
+
+switch (grade) {
+    case "A":
+        console.log("Achy grade ha malik");
+        break;
+
+    default:
+        console.log("Ghr walo sa mar khny k liya tarayar ho jao");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
